f549b279467e4019b71fbec51500d81a594fd32c
Steven Zheng <szheng.persevere@gmail.com>
2024-02-27 10:29:16 -0600

    made the scores flexible and display an incorrect if not true and correct if the right answer was ommited

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 // console.log(jeopardyQuestions.length)
 let count = 0
 let rightAnswer = ""
+let score = 0
+let reward = 0
 const clickables = $(".tiles")
 const questions = $("#question")
+const yourScore = $('p')
 clickables.on("click",(event)=>{
     console.log(event.target.innerHTML)
     if(count === 0){
-        let reward = event.target.innerHTML
+        reward = event.target.innerHTML
         //price range
         let targetQuestions = []
         //an array of questions that has the price range of the event clicked
@@ -17,20 +20,31 @@ clickables.on("click",(event)=>{
         }
         //pools specific questions for the generator to give
         let randomize = Math.floor(Math.random()*targetQuestions.length)
-        console.log(targetQuestions)
         let newQuestion = $(`<p>${targetQuestions[randomize-1].question}</p>`)
         rightAnswer =`${targetQuestions[randomize-1].answer}`
-        console.log(rightAnswer)
-
         //selects one of the multiple questions within range and also the right answer for the question
-        console.log(newQuestion)
         questions.append(newQuestion)
+        console.log(rightAnswer)
         //adds the question to the browser
+        targetQuestions.length = 0
+        //resets the questions
     }
     count++
+
 })
 
-// $("button").on("click",()=>{
-//     count= 0
-    
-// })
+$("button").on("click",()=>{
+    if(count>0){
+        if($("input").val() === "m"){
+            questions.text("Correct!!")
+            score += parseInt(reward.split("$")[1])
+            console.log(score)
+            yourScore.text(`Your Score:$${score}`)
+        } else {
+            questions.text("InCorrect!!")
+            score -= parseInt(reward.split("$")[1])
+            yourScore.text(`Your Score:$${score}`)
+        }
+    }
+    count = 0
+})
