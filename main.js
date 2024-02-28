9c11ea5bbf1c29b8c8e15f4885c58602cf17c425
Steven Zheng <szheng.persevere@gmail.com>
2024-02-28 10:34:59 -0600

    fixed case sensitivity not sure what else needs to be changed, website is fully functional and adjustable

--- main.js
+++ main.js
@@ -87,6 +87,7 @@ function loadGame (){
                 let selection = cat5.filter((obj)=> obj.value === reward)
                 $("#question").text(selection[0].question)
                 rightAnswer = selection[0].answer
+                console.log(rightAnswer)
             }
             //loads the right question on the screen
             event.target.innerHTML = ""
@@ -98,7 +99,7 @@ function loadGame (){
     })
     $("button").on("click",()=>{
         if(count>0){
-            if($("input").val() === rightAnswer){
+            if($("input").val().toLowerCase() === rightAnswer.toLowerCase()){
                 questions.text("Correct !!")
                 score += parseInt(reward.split("$")[1].replace(",",""))
                 yourScore.text(`YOUR SCORE:$${score}`)
@@ -113,64 +114,3 @@ function loadGame (){
     })
 }
 loadGame()
-
-
-
-
-
-
-
-
-//next thing to do is to generate random categories for each of the 100 - 500
-//og copy
-// let count = 0
-// let rightAnswer = ""
-// let score = 0
-// let reward = 0
-// const clickables = $(".tiles")
-// const questions = $("#question")
-// const yourScore = $('#dollarAmount')
-
-// clickables.on("click",(event)=>{
-//     console.log(event.target.innerHTML)
-//     if(count === 0){
-//         reward = event.target.innerHTML
-//         //price range
-//         let targetQuestions = []
-//         //an array of questions that has the price range of the event clicked
-//         for(let obj of jeopardyQuestions){
-//             if(obj.value === reward){
-//                 targetQuestions.push(obj)
-//             }
-//         }
-//         //pools specific questions for the generator to give
-//         let randomize = Math.floor(Math.random()*targetQuestions.length)
-//         let newQuestion = targetQuestions[randomize-1].question
-//         rightAnswer =`${targetQuestions[randomize-1].answer}`
-//         //selects one of the multiple questions within range and also the right answer for the question
-//         console.log(newQuestion)
-//         questions.text(newQuestion)
-//         console.log(rightAnswer)
-//         //adds the question to the browser
-//         targetQuestions.length = 0
-//         //resets the questions
-//         event.target.innerHTML = ""
-//         event.target.style.pointerEvents = "none"
-//     }
-//     count++
-// })
-
-// $("button").on("click",()=>{
-//     if(count>0){
-//         if($("input").val() === rightAnswer){
-//             questions.text("Correct!!")
-//             score += parseInt(reward.split("$")[1])
-//             yourScore.text(`YOUR SCORE:$${score}`)
-//         } else {
-//             questions.text("InCorrect!!")
-//             score -= parseInt(reward.split("$")[1])
-//             yourScore.text(`YOUR SCORE:$${score}`)
-//         }
-//     }
-//     count = 0
-// })
